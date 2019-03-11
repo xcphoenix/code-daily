cf988a5fe7222b44cb94681d06bbfeec5b143a95
PhoenixXC <bmingyy@qq.com>
2019-03-11 17:59:23 +0800

    SimpleWeibo: 使用 Maven | 修改数据库结构 | 修复消息超出消息框的问题

--- JSP_Servlet/SimpleWeibo/src/cc/xuanc/model/create_users.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE IF NOT EXISTS `users` (
-                                     `username` VARCHAR(16) NOT NULL,
-                                     `password` VARCHAR(16) NOT NULL,
-                                     `email` VARCHAR(16) NOT NULL,
-                                     PRIMARY KEY (`username`)
-)ENGINE=InnoDB DEFAULT CHARSET=utf8;
