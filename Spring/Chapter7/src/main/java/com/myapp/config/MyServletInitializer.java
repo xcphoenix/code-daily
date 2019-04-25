819f283950c3624bb26dfa700d9ff95fb8df3c56
PhoenixXC <bmingyy@qq.com>
2019-04-25 16:13:16 +0800

    some..

---
+++ Spring/Chapter7/src/main/java/com/myapp/config/MyServletInitializer.java
@@ -0,0 +1,18 @@
+package com.myapp.config;
+
+import javax.servlet.ServletException;
+import javax.servlet.ServletContext;
+import javax.servlet.ServletRegistration.Dynamic;
+import org.springframework.web.WebApplicationInitializer;
+
+/**
+ * ClassName    Chapter7-MyServletInitializer
+ * Description  通过实现 WebApplicationInitializer 来注册 Servlet
+ *
+ * @author      xuanc
+ * @date        19-4-24 上午8:48
+ * @version     1.0
+ */ 
+public class MyServletInitializer {
+
+}
