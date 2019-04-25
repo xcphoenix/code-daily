c484925c7af6be3acf66883919ac43c7f982cd71
PhoenixXC <bmingyy@qq.com>
2019-04-25 16:15:35 +0800

    some..

---
+++ JavaEE/Chapter2/src/main/java/com/lean/ssm/chapter2/proxy/jdk/HelloWorld.java
@@ -0,0 +1,13 @@
+package com.lean.ssm.chapter2.proxy.jdk;
+
+/**
+ * ClassName    Chapter2-HelloWorld
+ * Description  JDK 动态代理（由 java.lang.reflect.* 提供）
+ *
+ * @author      xuanc
+ * @date        19-4-25 下午1:49
+ * @version     1.0
+ */ 
+public interface HelloWorld {
+    public void sayHelloWorld();
+}
---
+++ JavaEE/Chapter2/src/main/java/com/lean/ssm/chapter2/proxy/jdk/HelloWorldImp1.java
@@ -0,0 +1,16 @@
+package com.lean.ssm.chapter2.proxy.jdk;
+
+/**
+ * ClassName    Chapter2-HelloWorldImp1
+ * Description  
+ *
+ * @author      xuanc
+ * @date        19-4-25 下午1:50
+ * @version     1.0
+ */ 
+public class HelloWorldImp1 implements HelloWorld {
+    @Override
+    public void sayHelloWorld() {
+        System.out.println("Hello World");
+    }
+}
