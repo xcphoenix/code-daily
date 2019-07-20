f608edcfd7243ec8cf072abbd91769fd49285f72
PhoenixXC <bmingyy@qq.com>
2019-07-20 11:29:27 +0800

    [SpringBoot-ShangGuiG] :: SpringBoot 配置

---
+++ SpringBoot-ShangGuiG/spring-boot-02-config-02/src/main/java/com/xuanc/springboot02config02/controller/HelloController.java
@@ -0,0 +1,13 @@
+package com.xuanc.springboot02config02.controller;
+
+/**
+ * ClassName    spring-boot-02-config-02-HelloController
+ * Description  
+ *
+ * @author      xuanc
+ * @date        2019/7/20 上午8:47
+ * @version     1.0
+ */ 
+public class HelloController {
+
+}
