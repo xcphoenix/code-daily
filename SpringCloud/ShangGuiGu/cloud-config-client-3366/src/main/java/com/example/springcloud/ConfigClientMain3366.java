2db07943e956ff4f225b0ba329aefa5eadbd8ef2
PhoenixXC <bmingyy@qq.com>
2020-05-18 16:29:04 +0800

    springcloud Bus

---
+++ SpringCloud/ShangGuiGu/cloud-config-client-3366/src/main/java/com/example/springcloud/ConfigClientMain3366.java
@@ -0,0 +1,20 @@
+package com.example.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+/**
+ * @author      xuanc
+ * @date        2020/5/18 上午11:15
+ * @version     1.0
+ */
+@EnableEurekaClient
+@SpringBootApplication
+public class ConfigClientMain3366 {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigClientMain3366.class, args);
+    }
+
+}
