0431ba8ea732084aa9da9520d2883ee337a04215
xcphoenix <root@xcphoenix.top>
2021-01-29 11:59:57 +0800

    bcc: first bcc

---
+++ C/bcc/src/go.mod
@@ -0,0 +1,5 @@
+module main
+
+go 1.15
+
+require github.com/iovisor/gobpf v0.0.0-20210109143822-fb892541d416 // indirect
--- C/module/src/main.c
+++ C/module/src/main.c
@@ -16,40 +16,41 @@ static struct kprobe kp = {
 };
 
 static int handler_pre(struct kprobe* p, struct pt_regs* regs) {
-  log_info("<%s> pre_handler: p->addr = %pF, ip = %lx, flags = 0x%lx\n",
-           p->symbol_name, p->addr, regs->ip, regs->flags);
-  return 0;
+    log_info("<%s> pre_handler: p->addr = %pF, ip = %lx, flags = 0x%lx\n",
+             p->symbol_name, p->addr, regs->ip, regs->flags);
+    log_info("argument maybe <%d>\n", ((struct task_struct* )((unsigned long )regs + 61))->pid);
+    return 0;
 }
 
 static void handler_post(struct kprobe* p, struct pt_regs* regs,
                          unsigned long flags) {
-  log_info("<%s> post_handler: p->addr = %pF, flags = 0x%lx\n", p->symbol_name,
-           p->addr, regs->flags);
+    log_info("<%s> post_handler: p->addr = %pF, flags = 0x%lx\n", p->symbol_name,
+             p->addr, regs->flags);
 }
 
 static int handler_fault(struct kprobe* p, struct pt_regs* regs, int trapnr) {
-  log_info("fault_handler: p->addr = %pF, trap #%dn", p->addr, trapnr);
-  return 0;
+    log_info("fault_handler: p->addr = %pF, trap #%dn", p->addr, trapnr);
+    return 0;
 }
 
 static int __init kprobe_init(void) {
-  int ret;
-  kp.pre_handler = handler_pre;
-  kp.post_handler = handler_post;
-  kp.fault_handler = handler_fault;
+    int ret;
+    kp.pre_handler = handler_pre;
+    kp.post_handler = handler_post;
+    kp.fault_handler = handler_fault;
 
-  ret = register_kprobe(&kp);
-  if (ret < 0) {
-    log_error("register_kprobe failed, returned %d\n", ret);
-    return ret;
-  }
-  log_info("Planted kprobe at %pF\n", kp.addr);
-  return 0;
+    ret = register_kprobe(&kp);
+    if (ret < 0) {
+        log_error("register_kprobe failed, returned %d\n", ret);
+        return ret;
+    }
+    log_info("Planted kprobe at %pF\n", kp.addr);
+    return 0;
 }
 
 static void __exit kprobe_exit(void) {
-  unregister_kprobe(&kp);
-  log_info("kprobe at %pF unregistered\n", kp.addr);
+    unregister_kprobe(&kp);
+    log_info("kprobe at %pF unregistered\n", kp.addr);
 }
 
 module_init(kprobe_init);
