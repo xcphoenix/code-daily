14696ed676e4d28a920d538cb4fe85238eb8ed1d
xcphoenix <root@xcphoenix.top>
2021-04-26 12:15:02 +0800

    the-rust-programming-language: ch15

--- Rust/the-rust-programming-language/ch15/rc/src/main.rs
+++ Rust/the-rust-programming-language/ch15/rc/src/main.rs
@@ -9,6 +9,12 @@ enum List {
 
 fn main() {
     let a = Rc::new(Cons(5, Rc::new(Cons(10, Rc::new(Nil)))));
+    println!("count after creating a = {}", Rc::strong_count(&a));
     let b = Cons(3, Rc::clone(&a));
-    let c = Cons(4, Rc::clone(&a)); // 引用计数+1并共享所有权
+    println!("count after creating b = {}", Rc::strong_count(&a));
+    {
+        let c = Cons(4, Rc::clone(&a)); // 引用计数+1并共享所有权
+        println!("count after creating c = {}", Rc::strong_count(&a));
+    }
+    println!("count after c goes out of scope = {}", Rc::strong_count(&a));
 }
